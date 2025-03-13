11e61e73657ba42e380e0c11434122dae3171542
Scr00b1e <kbolot312@gmail.com>
2025-03-13 14:09:55 +0600

    new

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 import telebot
-import webbrowser
+from telebot import types
 
 bot = telebot.TeleBot('8120813282:AAEZAZlQz_YhK7yY96hjWRX-GNIn2LYGC2Q')
 
-#@bot.message_handler(commands=['site', 'website'])
-#def site(message):
-#   webbrowser.open('https://itproger.com') 
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn2 = types.KeyboardButton('Delete photo')
+    btn3 = types.KeyboardButton('Edit text')
+    markup.row(btn1)
+    btn1 = types.KeyboardButton('Enter to site 😁')
+    markup.row(btn2, btn3)
+    file = open('./diditelluthatimissu.mp3')
+    bot.send_audio(message.chat.id, file, reply_markup=markup)
+    #file = open('./image.png', 'rb')
+    #bot.send_photo(message.chat.id, file, reply_markup=markup)
+    bot.send_message(message.chat.id, 'Hi', reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
 
-@bot.message_handler(commands=['start', 'main', 'hello'])
-def main(message):
-    bot.send_message(message.chat.id, f'Hi, {message.from_user.username} !')
+def on_click(message):
+    if message.text == 'Enter to site':
+        bot.send_message(message.chat.id, 'Website is open')
+    elif message.text == 'Delete photo':
+        bot.send_message(message.chat.id, 'Delete')
 
-@bot.message_handler(commands=['help'])
-def main(message):
-    bot.send_message(message.chat.id, 'Help information', parse_mode='html')
 
-@bot.message_handler()
-def info(message):
-    if message.text.lower() == 'hello':
-      bot.send_message(message.chat.id, f'Hi, {message.from_user.username} !')  
-    elif message.text.lower() == 'id':
-        bot.reply_to(message, f'ID: {message.from_user.id}')
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Delete photo', callback_data='delete')
+    btn2 = types.InlineKeyboardButton('Edit text', callback_data='edit')
+    markup.add(types.InlineKeyboardButton('Enter to site', url='https://monkeytype.com/'))
+    markup.row(btn1, btn2)
+    bot.reply_to(message, 'Such an amazing photo!', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'delete':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+    elif callback.data == 'edit':
+        bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
 
 
 bot.polling(non_stop=True)
